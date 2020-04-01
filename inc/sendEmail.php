9dda15d337c8e43b1c102bbc4136ef958af10699
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-04-01 18:47:47 +0530

    PHP form diabled

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'vivek.kaushal@outlook.com';
 
 
 if($_POST) {
--- js/main.js
+++ js/main.js
@@ -331,7 +331,7 @@
         $('#contactForm').validate({
 
             /* submit via ajax */
-            // submitHandler: function(form) {
+            // submitHandler: function (form) {
 
             //     var sLoader = $('.submit-loader');
 
@@ -340,31 +340,31 @@
             //         type: "POST",
             //         url: "inc/sendEmail.php",
             //         data: $(form).serialize(),
-            //         beforeSend: function() { 
+            //         beforeSend: function () {
 
             //             sLoader.slideDown("slow");
 
             //         },
-            //         success: function(msg) {
+            //         success: function (msg) {
 
             //             // Message was sent
             //             if (msg == 'OK') {
-            //                 sLoader.slideUp("slow"); 
+            //                 sLoader.slideUp("slow");
             //                 $('.message-warning').fadeOut();
             //                 $('#contactForm').fadeOut();
             //                 $('.message-success').fadeIn();
             //             }
             //             // There was an error
             //             else {
-            //                 sLoader.slideUp("slow"); 
+            //                 sLoader.slideUp("slow");
             //                 $('.message-warning').html(msg);
             //                 $('.message-warning').slideDown("slow");
             //             }
 
             //         },
-            //         error: function() {
+            //         error: function () {
 
-            //             sLoader.slideUp("slow"); 
+            //             sLoader.slideUp("slow");
             //             $('.message-warning').html("Something went wrong. Please try again.");
             //             $('.message-warning').slideDown("slow");
 
